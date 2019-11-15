361fe2c516f06d229e9a483b388b123c9c5e3e8f
Sidney Hopson <sidneydhopson@gmail.com>
2019-11-15 13:25:04 -0500

    Initial Project
    Added Project Template

---
+++ VA_Patient_Registration_Site/VA_Patient_Registration_Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VA_Patient_Registration_Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
